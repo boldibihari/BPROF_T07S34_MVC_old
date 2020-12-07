e6913bdbd9d6d51c7108398e7ce0481d8b502221
Bihari Tamás Boldizsár <biharibr@gmail.com>
2020-12-07 15:55:10 +0100

    Index

---
+++ NB1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Logic;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NB1.Controllers
+{
+    public class HomeController : Controller
+    {
+        ClubLogic clubLogic;
+        PlayerLogic playerLogic;
+        ManagerLogic managerLogic;
+
+        public HomeController(ClubLogic clubLogic, PlayerLogic playerLogic, ManagerLogic managerLogic)
+        {
+            this.clubLogic = clubLogic;
+            this.playerLogic = playerLogic;
+            this.managerLogic = managerLogic;
+        }
+
+        public IActionResult Init()
+        {
+            clubLogic.FillDbWithSamples();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
